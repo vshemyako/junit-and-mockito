3d9de7ded6c50ce65034391f67eb05aa8ac740af
Valentine Shemyako <v.shemyako@gmail.com>
2018-07-15 12:53:29 +0300

    Extracted local variables to method params

--- src/test/java/tdd/booking/BookingPeriodTest.java
+++ src/test/java/tdd/booking/BookingPeriodTest.java
@@ -1,20 +1,36 @@
 package tdd.booking;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.time.LocalTime;
 
 /**
  * Tests functionality of a {@link BookingPeriod} class
  */
+@RunWith(JUnitParamsRunner.class)
 public class BookingPeriodTest {
 
-    @Test
-    public void shouldInitializeFromAndToFields() {
-        LocalTime from = LocalTime.of(1, 0);
-        LocalTime to = LocalTime.of(2, 0);
+    /**
+     * @return pairs of from-to periods
+     */
+    private static Object[] getFromToPeriods() {
+        return new Object[][]{
+                {LocalTime.of(1, 0), LocalTime.of(2, 0)},
+                {LocalTime.of(4, 0), LocalTime.of(10, 0)},
+                {LocalTime.of(17, 0), LocalTime.of(23, 0)},
+        };
+    }
 
+    /**
+     * Verifies that during object construction {@code from} and {@code to} periods are set
+     */
+    @Test
+    @Parameters(method = "getFromToPeriods")
+    public void shouldInitializeFromAndToFields(LocalTime from, LocalTime to) {
         BookingPeriod bookingPeriod = new BookingPeriod(from, to);
         Assert.assertEquals("Constructor doesn't set 'from' point of time", from, bookingPeriod.getFrom());
         Assert.assertEquals("Constructor doesn't set 'to' point of time", to, bookingPeriod.getTo());
