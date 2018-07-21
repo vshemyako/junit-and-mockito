5f5a2149b0a8d2b77d119217b17abef2d81a340c
Valentine Shemyako <v.shemyako@gmail.com>
2018-07-21 10:02:33 +0300

    Added verification of adding the same period twice

--- src/main/java/tdd/booking/BookingSystem.java
+++ src/main/java/tdd/booking/BookingSystem.java
@@ -1,11 +1,15 @@
 package tdd.booking;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Offers booking several hours for defined booking target
  */
 public class BookingSystem {
 
     private final String bookingTarget;
+    private final Set<BookingPeriod> bookedPeriods;
 
     /**
      * Initializes instance of {@link BookingSystem} class with {@code bookingTarget}
@@ -21,6 +25,7 @@ public class BookingSystem {
         }
 
         this.bookingTarget = bookingTarget;
+        this.bookedPeriods = new HashSet<>();
     }
 
     /**
@@ -28,5 +33,14 @@ public class BookingSystem {
      */
     public String getBookingTarget() {
         return this.bookingTarget;
+    }
+
+    /**
+     * Books specified period
+     *
+     * @return {@code true} in case {@code bookingPeriod} wasn't previously booked
+     */
+    public boolean bookPeriod(BookingPeriod bookingPeriod) {
+        return bookedPeriods.add(bookingPeriod);
     }
 }
--- src/test/java/tdd/booking/BookingSystemTest.java
+++ src/test/java/tdd/booking/BookingSystemTest.java
@@ -3,8 +3,11 @@ package tdd.booking;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.time.LocalTime;
 
 /**
  * Tests booking functionality offered by {@link BookingSystem} class
@@ -13,11 +16,26 @@ import org.junit.runner.RunWith;
 public class BookingSystemTest {
 
     private static final String BOOKING_TARGET = "Hotel California";
+    private static final BookingPeriod VALID_PERIOD;
+
+    static {
+        VALID_PERIOD = new BookingPeriod(LocalTime.of(1, 0), LocalTime.of(2, 0));
+    }
 
     private static Object[] getInvalidBookingTargets() {
         return new Object[]{
                 null, "", "  "
         };
+    }
+
+    private BookingSystem bookingSystem;
+
+    /**
+     * Test fixture element - initializes fields required by test before each test invocation
+     */
+    @Before
+    public void setUp() {
+        this.bookingSystem = new BookingSystem(BOOKING_TARGET);
     }
 
     /**
@@ -38,5 +56,25 @@ public class BookingSystemTest {
     @Parameters(method = "getInvalidBookingTargets")
     public void constructorShouldThrowExceptionOnBlankBookingTarget(String bookingTarget) {
         new BookingSystem(bookingTarget);
+    }
+
+    /**
+     * Verifies that non previously booked {@link BookingSystem} should allow to book
+     * any valid {@link BookingPeriod}
+     */
+    @Test
+    public void shouldAllowBookingAnyPeriodOnFreshBookingSystem() {
+        boolean booked = bookingSystem.bookPeriod(VALID_PERIOD);
+        Assert.assertTrue(booked);
+    }
+
+    /**
+     * Verifies that previously booked period cannot be booked twice
+     */
+    @Test
+    public void shouldForbidBookingTheSamePeriodTwice() {
+        bookingSystem.bookPeriod(VALID_PERIOD);
+        boolean booked = bookingSystem.bookPeriod(VALID_PERIOD);
+        Assert.assertFalse(booked);
     }
 }
